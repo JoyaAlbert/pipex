afe512f670d9dcff9aa7499a5712eef109fd2152
JoyaAlbert <albertojoyapincu2@gmail.com>
2024-04-02 15:50:21 +0200

    s

---
+++ bonus/utils/ft_strjoin.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joya <joya@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 14:31:27 by ajoya-pi          #+#    #+#             */
+/*   Updated: 2024/03/31 12:47:19 by joya             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pipex.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char			*total;
+	unsigned int	i;
+	unsigned int	size;
+	unsigned int	size1;
+
+	i = 0;
+	size = ft_strlen(s1);
+	size1 = ft_strlen(s2);
+	total = (char *)malloc((size * sizeof(char)) + (size1 * sizeof(char)) + 1);
+	if (total == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		total[i] = s1[i];
+		i++;
+	}
+	size = 0;
+	while (size < size1)
+	{
+		total[i] = s2[size];
+		i++;
+		size++;
+	}
+	total[i] = '\0';
+	return (total);
+}
